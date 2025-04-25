23601e3c151ed7f55166cf2482878bb7dcc4593d
Erradilatifa <erradilatifa6@gmail.com>
2025-04-25 17:04:53 +0100

    Premier commit

---
+++ lib/pages/home.page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer:Drawer(
+        child: Column(
+          children:[
+            DrawerHeader(
+              child: Row(
+                mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                children: [
+                  CircleAvatar(
+                    backgroundImage:AssetImage("images/profile1.jpeg"),
+                    radius:50,
+                  ),
+                  CircleAvatar(
+                    backgroundImage:AssetImage("images/profile1.jpeg"),
+                    radius:22,
+                  ),
+                ],
+
+              ),
+              decoration:BoxDecoration(
+                gradient:LinearGradient(
+                  colors:[Colors.black,Theme.of(context).primaryColor],
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text("Home",style: Theme.of(context).textTheme.bodyLarge),
+              onTap:(){
+                Navigator.of(context).pop();
+                Navigator.pushNamed(context,"/");
+              } ,
+            ),
+            Divider(),
+            ListTile(
+              title: Text("Counter",style: Theme.of(context).textTheme.bodyLarge),
+              onTap:(){
+                Navigator.of(context).pop();
+                Navigator.pushNamed(context,"/counter");
+
+              } ,
+            ),
+            Divider(),
+            ListTile(
+              title: Text("Product",style: Theme.of(context).textTheme.bodyLarge),
+              onTap:(){
+                Navigator.of(context).pop();
+                Navigator.pushNamed(context,"/product");
+
+              } ,
+            ),
+
+          ],
+        ),
+
+      ) ,
+      appBar:AppBar(title:Text("home",style:TextStyle(color:Theme.of(context).indicatorColor) ,),
+        backgroundColor:Theme.of(context).primaryColor,) ,
+      body: Center(
+        child: Text("Home"),
+      ),
+    );
+
+  }
+}
